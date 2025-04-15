5ba8ec9ec46a0afc7276ee14b395c72b61ed6c19
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-15 16:52:04 +0530

    Ecommerce

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express=require('express');
 const app=express();
 const port=3000;
 
-const studentsRoutes=require('./routes/students');
-const coursesRoutes=require('./routes/courses');
+const usersRoutes=require('./routes/user');
+const productsRoutes=require('./routes/product');
+const cartRoutes=require('./routes/cart');
 
 app.get('/',(req,res)=>{
-  res.send("Welcome to the Student & Course Portal API!");
+  res.send("Welcome to the E-Commerce API");
 })
 
-app.use('/students',studentsRoutes);
-app.use('/courses',coursesRoutes);
+app.use('/user',usersRoutes);
+app.use('/product',productsRoutes);
+app.use('/cart',cartRoutes);
 
 app.use((req, res) => {
   res.status(404).send("Page not found");
-});
+})
+
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:${port}`)
 })
